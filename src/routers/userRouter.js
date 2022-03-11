5c537084b342f74fd562a149573c57e089bf9b58
junhee jang <jhjang.vdrc@gmail.com>
2022-03-11 18:00:52 +0900

    commit

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { edit,remove,logout,see } from "../controllers/userControllers.js";
+import { edit,remove,logout,see,startGithubLogin,finishGithubLogin } from "../controllers/userControllers.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout",logout);
 userRouter.get("/edit",edit);
 userRouter.get("/delete",remove);
+userRouter.get("/github/start",startGithubLogin);
+userRouter.get("/github/finish",finishGithubLogin);
 userRouter.get(":id",see);
 
 export default userRouter;
